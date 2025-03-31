7585435eee4143efa2a23f0698b1ba690a4c867d
김태현 <xogusrla7@gmail.com>
2025-03-31 15:37:55 +0900

    test2

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,9 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("안녕하쇼");
+        System.out.println("안녕하쇼");
+        System.out.println("안녕하쇼");
+        System.out.println("안녕하쇼");
+        System.out.println("안녕하쇼");
     }
 }
